162ec803829a4a6cbc5d967ce2dea1b09ce767c7
Barba02 <pippobarba02@gmail.com>
2022-02-11 10:38:14 +0100

    new standards

--- rand_min_datapath/randomizer.py
+++ rand_min_datapath/randomizer.py
@@ -5,8 +5,8 @@ from os.path import exists
 
 
 # statistiche riferimento min_datapath.blif
-min_nodes = 41
-min_lits = 207
+min_nodes = 44
+min_lits = 201
 # lista dei comandi di sis per la sintesi
 commands = ["source script.rugged", "eliminate -1", "sweep", "fx", "resub", "simplify", "full_simplify", "collapse",
             "reduce_depth", "espresso"]
